8a4cdbb949d158f42ca930ceecfe4d05090d4962
Johnson979 <toddjohnson@Todds-Mac-mini.local>
2015-02-26 13:30:45 -0600

    Initial Commit

---
+++ RVC PD/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RVC PD
+//
+//  Created by Todd Johnson on 2/26/15.
+//  Copyright (c) 2015 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
